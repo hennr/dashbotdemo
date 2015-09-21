a37825c5b1f991c8ed4dc46d616df2ad809b065f
Jan-Hendrik Peters <hennr@hennr.name>
2015-09-21 08:01:46 +0200

    init

---
+++ src/main/java/dashbotdemo/DashbotdemoApplication.java
@@ -0,0 +1,12 @@
+package dashbotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashbotdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashbotdemoApplication.class, args);
+    }
+}
---
+++ src/test/java/dashbotdemo/DashbotdemoApplicationTests.java
@@ -0,0 +1,18 @@
+package dashbotdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DashbotdemoApplication.class)
+@WebAppConfiguration
+public class DashbotdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
